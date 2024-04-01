531d54c02b780d972bc7f3ef4976134d69445e7a
duhuanwen <duhuanwen@163.com>
2024-04-01 18:04:33 +0800

    test

---
+++ src/views/test/index.tsx
@@ -0,0 +1,12 @@
+import Test from "../TS/project/one/2learn";
+
+const TestOut = () => {
+  return (
+    <div>
+      test
+      <br />
+      <Test />
+    </div>
+  );
+};
+export default TestOut;
